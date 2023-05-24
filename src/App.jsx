714af0d55c51397236346665119a5f8547871579
vegatality <vegatality@gmail.com>
2023-05-24 11:52:54 +0900

    backup

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from 'react';
-// import { QueryClient, QueryClientProvider } from 'react-query';
-// import Router from './shared/Router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Router from './shared/Router';
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0);
-  const something = 'hi';
-  if (something === 'hi') {
-    console.log('hi');
-  } else {
-    console.log('bye');
-  }
-  console.log('done');
-  useEffect(() => {
-    setInterval(() => {
-      console.log(`Count is: ${count}`);
-    }, 2000);
-  }, []);
+  // const [count, setCount] = useState(0);
+  // const something = 'hi';
+  // if (something === 'hi') {
+  //   console.log('hi');
+  // } else {
+  //   console.log('bye');
+  // }
+  // console.log('done');
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     console.log(`Count is: ${count}`);
+  //   }, 2000);
+  // }, []);
 
-  return (
-    <div>
-      {count}
-      <button type='button' onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
-    </div>
-  );
   // return (
-  //   <QueryClientProvider client={queryClient}>
-  //     <Router />
-  //   </QueryClientProvider>
+  //   <div>
+  //     {count}
+  //     <button type='button' onClick={() => setCount(count + 1)}>
+  //       Increase
+  //     </button>
+  //   </div>
   // );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
