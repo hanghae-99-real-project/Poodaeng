14ae805b05eef5d7832c3e4252d77e9e77a90f4f
vegatality <vegatality@gmail.com>
2023-05-25 12:57:59 +0900

    edit

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
     // <div className=' w-screen h-screen flex justify-center items-center bg-yellow-400'>
-    <div className=' w-max h-max flex justify-center items-center bg-yellow-400'>
+    <div className='w-fit h-fit flex justify-center items-center bg-yellow-400'>
       <div className='canvas'>
         <Outlet />
       </div>
