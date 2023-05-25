d51df64736fa1d45b20138136c40df94faa03f86
대식 <sejjang2eric@naver.com>
2023-05-25 12:52:13 +0900

    fix:세로만늘림

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <div className=' max-w-screen-sm h-screen flex justify-center items-center bg-yellow-400'>
+    <div className=' max-w-screen-sm h-screen-[110vh] flex justify-center items-center bg-yellow-400'>
       <div className='canvas'>
         <Outlet />
       </div>
