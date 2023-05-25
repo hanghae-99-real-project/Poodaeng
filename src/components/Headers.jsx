90e01ad8983b59799b1befc98b3dd2e03fa8cc8b
대식 <sejjang2eric@naver.com>
2023-05-25 17:26:59 +0900

    fix: 뒤로가기시 새로고침

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -3,13 +3,17 @@ import { useNavigate } from 'react-router-dom';
 
 function Headers({ children }) {
   const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate('/');
+    window.location.reload();
+  };
   return (
     <div className='relative w-full bg-[#FFFFFF] z-20 border-b shadow-sm mt-[29px]'>
       <img
         className='absolute left-5 scale-[-1] cursor-pointer mt-7'
         src='Vector 56.png'
         alt='<'
-        onClick={() => navigate('/')}
+        onClick={navigateHandler}
         role='none'
       />
       <div className='flex justify-center mt-[20px] mb-[20px] font-[700]'>
