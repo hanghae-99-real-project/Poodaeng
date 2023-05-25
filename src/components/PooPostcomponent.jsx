391e3a2f6aece637434981ad7100f9497e354b8f
대식 <sejjang2eric@naver.com>
2023-05-25 17:56:50 +0900

    feat:댕파인더 초기틀

--- src/components/PooPostcomponent.jsx
+++ src/components/PooPostcomponent.jsx
@@ -1,23 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Headers from './Headers';
 import FileUploader from './FileUploader';
+import Buttons from './common/Buttons';
 
 function PooPostcomponent() {
-  const navigate = useNavigate();
   return (
     <div className='container'>
-      <Headers>
-        <img
-          className='absolute left-5 scale-[-1] mt-1 cursor-pointer'
-          src='Vector 56.png'
-          alt='<'
-          onClick={() => navigate('/')}
-          role='none'
-        />
-        <div className='font-[700]'>푸박스 등록</div>
-      </Headers>
+      <Headers>푸박스 등록</Headers>
       <div className='ml-[23px] mt-[31px]'>
         <div className='font-[600] text-[15px]'>푸박스 위치</div>
         <input
@@ -35,6 +25,7 @@ function PooPostcomponent() {
           className='border-none font-[400] text-[15px] pb-[8px] mt-[13px] w-[330px] h-[200px] resize-none'
           placeholder='특이사항 입력'
         />
+        <Buttons className='ml-[23px]'>등록하기</Buttons>
       </div>
     </div>
   );
---
+++ src/pages/DaengFinder.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DaengFindercomponent from '../components/DaengFindercomponent';
+
+function DaengFinder() {
+  return <DaengFindercomponent />;
+}
+
+export default DaengFinder;
