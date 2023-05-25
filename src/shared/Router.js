5e7305aca1f4b0738d5a150a1cf17e55488c9b0d
대식 <sejjang2eric@naver.com>
2023-05-25 10:27:34 +0900

    feat: 0525작업전 푸쉬

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Layout from './Layout';
-<<<<<<< HEAD
 import Alert from '../pages/Alert';
 import Events from '../pages/Events';
 import Map from '../pages/Map';
 import PooPost from '../pages/PooPost';
-=======
-import SignupPage from '../pages/SignupPage';
+import LoginPage from '../pages/LoginPage';
 import SignInPage from '../pages/SignInPage';
->>>>>>> dbae0644d57babc0e64721ac7052c8291237c0f7
+import SignupPage from '../pages/SignupPage';
 
 function Router() {
   console.log("렌더링 발생하였습니다.")
@@ -19,16 +17,13 @@ function Router() {
       <Routes>
         <Route element={<Layout/>}>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/alert" element={<Alert />} />
           <Route path="/events" element={<Events />} />
           <Route path="/map" element={<Map />} />
           <Route path="/poopost" element={<PooPost />} />
-=======
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signin' element={<SignInPage />}/>
           <Route path='/signup' element={<SignupPage />}/>
->>>>>>> dbae0644d57babc0e64721ac7052c8291237c0f7
         </Route>
       </Routes>
     </BrowserRouter>
