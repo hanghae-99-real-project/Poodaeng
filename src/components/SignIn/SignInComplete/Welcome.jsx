083f42ad4ccc74eff7b4905cf8b569d978fa34f3
Olim <vegatality@gmail.com>
2023-06-15 11:03:49 +0900

    FIX: react-lottie(deprecated) is eliminated. changed to lottie-react(latest official version)

--- src/components/SignIn/SignInComplete/Welcome.jsx
+++ src/components/SignIn/SignInComplete/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Lottie from 'react-lottie';
+import Lottie from 'lottie-react';
 import WelcomeConfetti from '../../../assets/images/Welcome.json';
 
 const Welcome = () => {
@@ -12,7 +12,7 @@ const Welcome = () => {
     },
   };
 
-  return <Lottie options={defaultOptions} />;
+  return <Lottie {...defaultOptions} />;
 };
 
 export default Welcome;
