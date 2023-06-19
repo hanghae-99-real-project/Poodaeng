072a4bfb4699ee7eec66b00255667c84e88591a1
Olim <vegatality@gmail.com>
2023-06-19 23:06:15 +0900

    TEST: main.js axios 콜 변경 테스트

--- src/api/main.js
+++ src/api/main.js
@@ -1,13 +1,17 @@
 import axiosToken from "./axiosToken";
 
 // 댕파인더 메인화면 조회
+// const getDaengMain = async () => {
+//   try {
+//     const response = await axiosToken.get(`/api/main/lostposts`)
+//     return response
+//   } catch (error) {
+//     return Promise.reject(error.data)
+//   }
+// };
 const getDaengMain = async () => {
-  try {
     const response = await axiosToken.get(`/api/main/lostposts`)
     return response
-  } catch (error) {
-    return Promise.reject(error.data)
-  }
 };
 
 export default getDaengMain
