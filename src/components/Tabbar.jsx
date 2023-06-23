fd959ddbaab75a255ea4ef194ea4674adaef7ccc
대식 <sejjang2eric@naver.com>
2023-06-23 15:13:01 +0900

    fix:app.jsx 수정

--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -61,6 +61,7 @@ function Tabbar({ setIsActive }) {
   const handleIconClick = route => {
     navigate(route);
   };
+
   return (
     <div className='sticky bottom-0 bg-[#FFFFFF] z-20'>
       <div className='flex justify-evenly items-center w-[375px] h-[65px] shadow-sm pb-5'>
