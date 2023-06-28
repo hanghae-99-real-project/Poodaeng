15eacbe8bf557811d01eedaa5b9ee4f49c3c543a
대식 <sejjang2eric@naver.com>
2023-06-28 15:30:36 +0900

    fix: 슬라이드 수정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './tailwind.css';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/config/configStore';
+import './tailwind.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <App />
-  </Provider>
+  // </Provider>
 );
